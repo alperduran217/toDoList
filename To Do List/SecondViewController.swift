8f1c254a4ae0756b74f58ef19446cfc698a54789
alperduran217 <alperduran217@gmail.com>
2016-03-25 01:37:00 +0200

    Final work.

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        let content:String = item.text!
+        toDoList.append(content)
+        
+        item.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList") // Kalıcı olarak içerikleri kayıt editoyr
+        
+        
+    }
+    @IBOutlet weak var item: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.item.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,20 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+        // Boşluğa dokununca klavyeyi kaldırıyor
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        item.resignFirstResponder()
+        
+        return true
 
+    }
 }
 
